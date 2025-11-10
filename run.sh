94dc540e89e8585784c9e3821ff49971440bd86e
bfren <bf@bfren.dev>
2025-11-10 12:29:20 +0000

    v2.5.4 (#56)
    Minor updates
    * Updating Unbound build
    
    Development updates
    * Removing no cache and plain progress from run

--- run.sh
+++ run.sh
@@ -4,8 +4,6 @@ IMAGE=`cat VERSION`
 
 docker buildx build \
     --load \
-    --no-cache \
-    --progress plain \
     --build-arg BF_IMAGE=unbound \
     --build-arg BF_VERSION=${IMAGE} \
     -f Dockerfile \
